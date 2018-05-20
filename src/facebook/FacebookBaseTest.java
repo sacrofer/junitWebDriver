8dec40cd084ff4a7d9dbb9523f1705216e7cf994
sacrofer <38994980+sacrofer@users.noreply.github.com>
2018-05-20 17:16:16 -0500

    Updates to work locally

--- src/facebook/FacebookBaseTest.java
+++ src/facebook/FacebookBaseTest.java
@@ -5,20 +5,27 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FacebookBaseTest {
 	
 	protected WebDriver driver;
+	private static final String CHROME_DRIVER_NAME = "webdriver.chrome.driver";
+	private static final String GENERIC_DRIVER_LOCATION = "C:\\testautomation\\libs\\drivers\\";
+	private static final String CHROME_DRIVER_LOCATION = GENERIC_DRIVER_LOCATION + "chromedriver.exe";
+
 	
 	protected void setUp(String browser, String url) {
-		System.out.println("Arranca el browser porfa y navega a la pagina.");
-		driver = new ChromeDriver();
-		driver.get(url);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--disable-notifications");
+		System.setProperty(CHROME_DRIVER_NAME, CHROME_DRIVER_LOCATION);
+	    driver = new ChromeDriver(ops);		
+	    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	    driver.get(url);
 	}
 	
 	@After
-	protected void tearDown() {
+	public void tearDown() {
 		System.out.println("Destruye la configuracion");
 		driver.quit();
 	}
